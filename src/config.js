7e1ec065f5d35e9ee976a857e268b3acc1dd1aef
Glitch (redux-trello-router) <none>
2018-03-16 04:22:40 +0000

    deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8080,
     // other stuff
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||  "https://stormy-badlands-44481.herokuapp.com/"  
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||  "https://stormy-badlands-44481.herokuapp.com"  
     
     // "http://localhost:3006" 
     
